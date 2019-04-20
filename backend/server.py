9d515048ba19da430ea229127edab40f41097b84
ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-04-20 17:16:37 +0700

    change request type to json

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
 from .algo import answer
+import json
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def respond():
-    question = request.form['question']
-    response = answer(question)
+    question = request.get_json()
+    response = answer(question["question"])
     return jsonify({"question": question, "response" : response})

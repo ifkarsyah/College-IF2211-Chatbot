291a8c66ef64e22f1f8bcdeff541bcbe29b9b17a
ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-04-20 18:25:23 +0700

    resolve cors

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
 from .algo import answer
+
+from flask_cors import CORS
+
 import json
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def respond():
     question = request.get_json()
+    print(question)
     response = answer(question["question"])
-    return jsonify({"question": question, "response" : response})
+    return jsonify({"question": question["question"], "response": response})
--- frontend/index.php
+++ frontend/index.php
@@ -18,17 +18,33 @@
 
     <script type="text/javascript" src="static/js/jquery-3.4.0.min.js"></script>
     <script>
-
-      $("input[name='send']").click(function() {
-        var usermsg = $("#msg").val();
-        $.post("process.php",{msg: usermsg});
+    $("input[name='send']").click(function() {
+        dataReq = {
+          question : $("#msg").val(),
+        };
+        // console.log(JSON.stringify(dataReq));
+        $.ajax({
+            crossDomain: true,
+            type: 'post',
+            dataType: 'jsonp',
+            url : 'http://127.0.0.1:5000/',           
+            contentType: 'application/json;charset=UTF-8',
+            data: JSON.stringify(dataReq),
+            success: function() { 
+              alert("Success"); 
+            },  
+            error: function (xhr, status) {
+              alert("error");
+            }
+        });   
         $("#msg").attr("value", "");
-        return false;
-      });
+        return false;  
+    });
+
 
       function loadLog() {
         var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
-        $.ajax({
+          $.ajax({
           url: "log.html",
           cache: false,
           success: function(html) {

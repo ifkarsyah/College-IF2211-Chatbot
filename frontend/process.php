2ea7dc64c70de32d82d49fb79a63e9a7c28cd40b
ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-04-22 00:11:35 +0700

    rearrange

--- frontend/process.php
+++ frontend/process.php
@@ -6,21 +6,20 @@ $answer = $_POST['answer'];
 
 $fp = fopen("log.html", 'a') or die("can't open file");
 
-$bot_cont  = '<div class = "chat bot">';
 $user_cont = '<div class = "chat me">';
+$bot_cont  = '<div class = "chat bot">';
 
-$bot_img  = '<div class="user-photo"><img src="static/giphy.gif"></div>';
 $user_img = '<div class="user-photo"><img src="static/img/user.png"></div>';
+$bot_img  = '<div class="user-photo"><img src="static/giphy.gif"></div>';
 
-$bot_msg  = '<p class = "chat-message">' . $question . "</p>";
-$user_msg = '<p class = "chat-message">' . $answer . "</p>";
+$user_msg = '<p class = "chat-message">' . $question . "</p>";
+$bot_msg  = '<p class = "chat-message">' . $answer . "</p>";
 
 $user_full = $user_cont . $user_img . $user_msg . "</div>";
 $bot_full = $bot_cont . $bot_img . $bot_msg . "</div>";
 
-
-fwrite($fp, $bot_full);
 fwrite($fp, $user_full);
+fwrite($fp, $bot_full);
 
 fclose($fp);
 ?>
